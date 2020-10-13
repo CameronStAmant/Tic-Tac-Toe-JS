cb57323bb69034f204a25c03495df1a86ec6dc97
Cameron St. Amant <cameronstamant@protonmail.com>
2020-10-12 22:22:04 -0600

    finish step 6

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 'use strict'
 const gameBoard = (() => {
   const board = ['','','','','','','','',''];
-  const genBoard = document.getElementById('gameboard');
-  let a = 0;
-  for (let i = 0; i < board.length; i++) {
-  let i = document.createElement('div');
-  i.id = a;
-  a++;
-  genBoard.appendChild(i);
-  }
-
-  return {
-    board
+  const gen = () => {
+    let a = 0;
+    for (let i = 0; i < board.length; i++) {
+    let i = document.createElement('div');
+    i.id = a;
+    a++;
+    document.getElementById('gameboard').appendChild(i);
+    }
   };
-
+  gen();
+  return {
+    board,
+    gen
+  };
 })();
 
 const displayController = (() => {
+  let go = false;
   const score = {};
   let playerTurn = 'O';
-  const displayBoard = document.getElementById('gameboard');
-  
-  displayBoard.addEventListener('click', (e) => {
+  const actions = () => {
+    document.getElementById('gameboard').addEventListener('click', (e) => {
+      if (go == false) {
+        return;
+      } else {
     if (playerTurn == 'X') {
       if (e.target.innerHTML == '') {
         e.target.innerHTML = 'X';
@@ -41,23 +45,49 @@ const displayController = (() => {
         return;
       }
     }
-  });
-
+    }})
+  };
+  actions();
   const gameOver = (winner) => {
     if ((gameBoard.board[0] == 'X' && gameBoard.board[1] == 'X' && gameBoard.board[2] == 'X') || (gameBoard.board[3] == 'X' && gameBoard.board[4] == 'X' && gameBoard.board[5] == 'X') || (gameBoard.board[6] == 'X' && gameBoard.board[7] == 'X' && gameBoard.board[8] == 'X') || (gameBoard.board[0] == 'X' && gameBoard.board[3] == 'X' && gameBoard.board[6] == 'X') || (gameBoard.board[1] == 'X' && gameBoard.board[4] == 'X' && gameBoard.board[7] == 'X') || (gameBoard.board[2] == 'X' && gameBoard.board[5] == 'X' && gameBoard.board[8] == 'X') || (gameBoard.board[0] == 'X' && gameBoard.board[4] == 'X' && gameBoard.board[8] == 'X') || (gameBoard.board[2] == 'X' && gameBoard.board[4] == 'X' && gameBoard.board[6] == 'X')) {
-      alert('X player wins!');
+      alert(`${players.player1Name.value} wins! Congratulations!`);
+      for (let u = 0; u < gameBoard.board.length; u++) {
+        if (gameBoard.board[u] == '') {
+          document.getElementById(`${u}`).innerHTML = ' ';
+        }
+      }
     } else if ((gameBoard.board[0] == 'O' && gameBoard.board[1] == 'O' && gameBoard.board[2] == 'O') || (gameBoard.board[3] == 'O' && gameBoard.board[4] == 'O' && gameBoard.board[5] == 'O') || (gameBoard.board[6] == 'O' && gameBoard.board[7] == 'O' && gameBoard.board[8] == 'O') || (gameBoard.board[0] == 'O' && gameBoard.board[3] == 'O' && gameBoard.board[6] == 'O') || (gameBoard.board[1] == 'O' && gameBoard.board[4] == 'O' && gameBoard.board[7] == 'O') || (gameBoard.board[2] == 'O' && gameBoard.board[5] == 'O' && gameBoard.board[8] == 'O') || (gameBoard.board[0] == 'O' && gameBoard.board[4] == 'O' && gameBoard.board[8] == 'O') || (gameBoard.board[2] == 'O' && gameBoard.board[4] == 'O' && gameBoard.board[6] == 'O')) {
-      alert('O player wins!');
+      alert(`${players.player2Name.value} wins! Congratulations`);
+      for (let u = 0; u < gameBoard.board.length; u++) {
+        if (gameBoard.board[u] == '') {
+          document.getElementById(`${u}`).innerHTML = ' ';
+        }
+      }
     } else if (gameBoard.board.every(i => (i != ''))) {
       alert('Tie game!');
     }
   };
-
+  const startReset = document.getElementById('startReset')
+  startReset.addEventListener('click', (refresh) => {
+    go = true;
+    document.getElementById('gameboard').remove();
+    let createDiv = document.createElement('div');
+    createDiv.id = 'gameboard';
+    document.body.appendChild(createDiv);
+    gameBoard.gen();
+    gameBoard.board = ['','','','','','','','',''];
+    actions();
+  });
   return {
     score
   };
 })();
 
-const players = (name) => {
-  return { name };
-};
+const players = (() => {
+  let player1Name = document.getElementById('player1');
+  let player2Name = document.getElementById('player2')
+  return {
+    player1Name,
+    player2Name
+  };
+})();
